6433db4a066bb25d43b3ba1d07614ff122873bfd
Vaibhav Pratap Singh <pratapvaibhav446@gmail.com>
2026-06-05 22:24:30 +0530

    Added OOPS WEEK_1 Assignment

--- src/Week_1/OOPS/LibraryInterfaceDemo.java
+++ src/Week_1/OOPS/LibraryInterfaceDemo.java
@@ -1,5 +1,5 @@
+// main class to test the LibraryInterface implementation
 public class LibraryInterfaceDemo {
-
     public static void main(String[] args) {
 
         System.out.println("===== Test Case 1 : KidUser =====");
